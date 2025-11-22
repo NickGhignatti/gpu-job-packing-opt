0d3928de147da1f4cb66d5b44bba4bfc2cbea6f3
NickGhignatti <nick.ghignatti@gmail.com>
2025-11-22 21:50:06 +0100

    Start scheduler implementation

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -11,8 +11,8 @@ import (
 type Scheduler struct {
 	mu       sync.RWMutex
 	provider provider.GPUProvider
-	//	queue     *JobQueue
-	jobs map[string]*types.Job
+	queue    *types.JobQueue
+	jobs     map[string]*types.Job
 	//	algorithm Algorithm
 	//	predictor PredictorClient
 	logger *logrus.Logger
@@ -24,4 +24,42 @@ type Scheduler struct {
 type Config struct {
 	Algorithm    string
 	PredictorURL string
+}
+
+func New(provider provider.GPUProvider, config Config) (*Scheduler, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	logger := logrus.New()
+	logger.SetLevel(logrus.InfoLevel)
+
+	// TODO : algorithm selection (kinda best-fit or first-fit)
+	
+	s := &Scheduler{
+		mu:       sync.RWMutex{},
+		provider: provider,
+		queue:    types.NewJobQueue(),
+		jobs:     make(map[string]*types.Job),
+		logger:   logger,
+		ctx:      ctx,
+		cancel:   cancel,
+	}
+
+	if config.PredictorURL != "" {
+		// create predictor client if provided
+	}
+
+	return s, nil
+}
+
+func (s *Scheduler) SubmitJob(job *types.Job) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// TODO : should predict here
+
+	s.jobs[job.UUID] = job
+	s.queue.Enqueue(job)
+	s.logger.Infof("Job %s submitted (%s)", job.UUID, job.Name)
+
+	return nil
 }
